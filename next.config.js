a99dd086f6413984829437be84eb27c27baac994
Maninderthecoder <ms1920af@gmail.com>
2024-01-07 20:09:35 +0530

    nextjs dashboard starting code

---
+++ next.config.js
@@ -0,0 +1,8 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    // images: {
+    //     formats: ['image/avif', 'image/webp'],
+    // }, // avif take 20% more time to compress but makes 20% less in size than webp
+};
+
+module.exports = nextConfig;
